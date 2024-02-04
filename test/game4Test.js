f3a982fc4992e86da4eafa6ec71868b2f310c82d
Xhufg <Minardi.alessio99@gmail.com>
2024-02-04 23:26:45 +0100

    puzzles solved

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,19 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
+    const signer0 = ethers.provider.getSigner(0);
 
-    return { game };
+
+    return { game, signer0 };  
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-
+    const { game, signer0 } = await loadFixture(deployContractAndSetVariables);
+    console.log(await signer0.getAddress())
     // nested mappings are rough :}
+    await game.write(await signer0.getAddress())
 
-    await game.win();
+
+    await game.win(await signer0.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
